2598464f1d6c5f6be1ced83e0220dd75129d2313
devanhdq <dev.anhdq@gmail.com>
2024-11-02 12:39:37 -0700

    create home page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact.jsx";
+import MyProfile from "./pages/MyProfile.jsx";
+import MyAppointments from "./pages/MyAppointments.jsx";
+import Appointment from "./pages/Appointment.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+    return (
+        <div className="mx-4 sm:mx-[10%]">
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/doctors" element={<Doctors/>}/>
+                <Route path="/doctors/:specially" element={<Doctors/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/my-profile" element={<MyProfile/>}/>
+                <Route path="/my-appointments" element={<MyAppointments/>}/>
+                <Route path="/appointment/:doctorId" element={<Appointment/>}/>
+            </Routes>
+
+        </div>
+    );
+};
+
+export default App;
